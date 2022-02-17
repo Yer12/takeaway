60e06d15cf20d679b7de7594e74d9aadae504557
nurzhasj <montishok@gmail.com>
2022-02-17 20:48:03 +0600

    endopint for getting restaurants is completed

--- app/Http/Controllers/RestaurantsController.php
+++ app/Http/Controllers/RestaurantsController.php
@@ -10,6 +10,19 @@ class RestaurantsController extends Controller{
     public function index() : JsonResponse{
         $restaurants = Restaurant::all();
 
-        return response()->json($restaurants);
+        $res = [];
+
+        $inc = 1;
+        foreach ($restaurants as $restaurant){
+            $res[] = [
+                'restaurant_'.$inc => [
+                    'restaurant_data' => $restaurant,
+                    'image' => $restaurant->restaurantImages()->get()->first()
+                ]
+            ];
+            $inc+=1;
+        }
+
+        return response()->json($res);
     }
 }
